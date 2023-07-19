ec2422a91ff60e3914dd2b93f75755803b0d6c62
oyenet1 <bowofadeoyerinde@gmail.com>
2023-07-19 21:51:56 +0100

    ok done

--- pages/emojiapi.vue
+++ pages/emojiapi.vue
@@ -1,9 +1,22 @@
 <template>
- <div>
-  emoji page
+ <div class="bg-slate-200 w-full space-y-4 p-6 h-screen">
+  <div class="flex bg-white p-4 rounded-lg shadow justify-between max-w-5xl mx-auto items-center">
+   <h1 class="text-center text-xl uppercase font-medium">List of emojis</h1>{{ search }}
+   <div class="flex items-center space-x-2 text-sm">
+    <input v-model="search" type="text" class="bg-slate-50 border px-3 py-2 rounded-md" placeholder="categories">
+    <button class="bg-blue-500 text-white px-4 py-2 rounded-lg ">Search</button>
+   </div>
+  </div>
+  <div class="w-full  bg-white p-4 rounded-lg shadow max-w-5xl mx-auto"></div>
  </div>
 </template>
 
 <script setup>
+
+let search = ref('')
+
+let emojis = [];
+
+const { data: emoji } = await useFetch('https://emojihub.yurace.pro/api/all')
 
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
  <div class="bg-slate-200 py-4 h-screen w-screen">
   <div class="w-full max-w-lg space-y-4  p-4 shadow inset mx-auto bg-white rounded-lg oveflow-y-auto">
-   <h1 class="font-medium uppercase">The list of all my components app built using nuxtjs</h1>
+   <h1 class="font-medium uppercase bg-primary">The list of all my components app built using nuxtjs</h1>
    <div class="w-full">
     <ul class="list space-y-2 list-decimal list-inside">
      <li v-for="(cont, index) in dat" :key="index" class="rounded shadow space-y-1 bg-slate-50 p-2">
       <span @click="toggleList(cont.id)" class="w-full">{{ cont.title }}</span>
-      <p class="text-sm pl-5 text-gray-500" v-show="show == cont.id">
+      <p class="text-sm pl-5 text-gray-500 w-full" v-show="show == cont.id">
        {{ cont.content }}
        <span class="text-blue-600 italic">
         <NuxtLink :to="cont.link" target="_blank">here</NuxtLink>
@@ -29,10 +29,16 @@ function toggleList(val) {
 
 let dat = ref([
  {
-  id: 1, title: "Emoji Api", content: "This app consume emoji apis and list the on a page. The intermediate part of the app is that it can filter emojis base on category visit the link", link: "emojiapi"
+  id: 1,
+  title: "Emoji Api",
+  content: "This app consume emoji apis and list the on a page. The intermediate part of the app is that it can filter emojis base on category visit the link",
+  link: "emojiapi"
  },
  {
-  id: 2, title: "Color Picker", content: "it's an app to select and pick color and it display the color", link: "colorpicker"
+  id: 2,
+  title: "Color Picker",
+  content: "it's an app to select and pick color and it display the color",
+  link: "colorpicker"
  },
 ]);
 
